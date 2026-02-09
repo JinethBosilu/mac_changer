aec1bd49604d711bc7d24434ab3ad4a31bdc31a2
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-09 18:26:40 +0530

    changed the interface and new_mac variables to take user input instead of hardcoding values.

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,7 @@
 import subprocess
 
-interface = "eth0"
-new_mac = "00:11:22:33:44:55"
+interface = input("Enter the interface you want to change the MAC address for: ")
+new_mac = input("Enter the new MAC address: ")
 
 print("[+] Changing MAC address for "+interface+" to "+new_mac)
 
