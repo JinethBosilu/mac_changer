188bdad6754a1d4802d062759e0eda982b7116b8
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-09 20:51:20 +0530

    arguments added to the script

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,10 @@
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+(options, arguments) = parser.parse_args()
 
 interface = input("Enter the interface you want to change the MAC address for: ")
 new_mac = input("Enter the new MAC address: ")
